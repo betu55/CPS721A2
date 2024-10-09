e8875af81209ec9e958b9e543937298f90b9857c
soupe1 <77170782+soupe1@users.noreply.github.com>
2024-10-08 20:42:05 -0400

    Update q4_nested_find.pl
    tried fixing q4

--- q4_nested_find.pl
+++ q4_nested_find.pl
@@ -14,3 +14,13 @@
 
 %%%%% SECTION: nestedLists
 %%%%% Put your rules for nestedFindDepth, nestedFindIndex, and any helper predicates below
+
+nestedFindDepth(List, Item, 0):- List == Item.
+nestedFindDepth([H | T], Item, 0):- H == Item.
+nestedFindDepth([H | T], Item, Depth):- is_list(H), nestedFindDepth(H, Item, D1), Depth is D1 + 1.
+nestedFindDepth([H | T], Item, Depth):- nestedFindDepth(T, Item, Depth).
+
+nestedFindIndex(List, Item, 0, 0):- List == Item.
+nestedFindIndex([H | T], Item, 0, 0):- H == Item.
+nestedFindIndex([H | T], Item, Depth, Index):- is_list(H), nestedFindDepth(H, Item, D1), Depth is D1 + 1, nestedFindIndex(T, Item, Depth, Index).
+nestedFindDepth([H | T], Item, Depth, Index):- nestedFindIndex(T, Item, Depth, I1), Index is I1 + 1.
